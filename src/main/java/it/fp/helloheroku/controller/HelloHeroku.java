06890c02a957204c154462c77995fe550366ae6a
Picpippo <filippo.piccini@gmail.com>
2021-04-05 20:01:03 +0200

    only "/"

--- src/main/java/it/fp/helloheroku/controller/HelloHeroku.java
+++ src/main/java/it/fp/helloheroku/controller/HelloHeroku.java
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloHeroku {
 	
-	@RequestMapping(value ="/hello", method = RequestMethod.GET)
+	@RequestMapping(value ="/", method = RequestMethod.GET)
 	public String hello(Model model){
 		
 		return "hello";
